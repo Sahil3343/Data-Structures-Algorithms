d35242786a8eeda40240f17a4170bd31b9e979d2
Sahil Mahajan <sahil3343@gmail.com>
2022-03-02 22:55:47 +0530

    Initial Commit

---
+++ src/com/sahilmahajan/dsa/Main.java
@@ -0,0 +1,10 @@
+package com.sahilmahajan.dsa;
+
+public class Main {
+
+    public static void main(String[] args) {
+       SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+       //sortingAlgorithms.BubbleSort();
+       //sortingAlgorithms.SelectionSort();
+    }
+}
